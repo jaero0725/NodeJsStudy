594a901fd95a9fecd305419968dc1a17cf17090b
choijaeho <dhqkgbs@naver.com>
2022-11-27 01:00:51 +0900

    update : todoList  delete, insert

--- TODOAPP/server.js
+++ TODOAPP/server.js
@@ -23,12 +23,10 @@ MongoClient.connect(MONGO_DB_URL, function(err, database){
     db = database.db('todoapp');  // todoapp 이라는 db에 연결
 
     app.listen(PORT , () => {  
-        console.log('listening on ', PORT ,",",properties.get("MONGO_DB_URL"));
-        console.log(properties);
+        console.log('listening on ', PORT ," | " , properties.get("MONGO_DB_URL"));
     });
    
 });
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -38,15 +36,41 @@ app.get('/write', (req, res) => {
     res.sendFile(__dirname + '/write.html');
 });
 
-app.get('/list', (req, res) => {
-    res.render('list.ejs'); //views 에 넣어두어야됨
-});
-
+/** /add : 게시글 추가하기 */ 
 app.post('/add', (req, res) => {
     let todo = req.body; 
-    db.collection('post').insertOne(todo, (err, res) => {
-        console.log("저장 완료");
-    });  
+    db.collection("counter").findOne({name :'게시물갯수'}, (err, data) => {    // auto increment => mongo db는 없음 :직접구현
+        // counter : sequence 역할 
+        todo._id = data.totalPost;
 
+        db.collection('post').insertOne(todo, (err, data) => {
+            if (err) throw err;
+            console.log("저장 완료");
+        });  
+
+        db.collection("counter").updateOne({name :'게시물갯수'}, {$inc : {totalPost : 1}}, (err, data)  => {
+            if (err) console.log("error : sequence[counter] update error");
+            else console.log("1 document updated");
+        });
+        
+    });
     return res.redirect("/write");
 });
+
+/** /list : 전체 목록 보기*/ 
+app.get('/list', (req, res) => {
+    //db 에 저장된 post 라는 collection안의 모든 데이터를 꺼내주세요. 
+   db.collection("post").find().toArray((err, data)=>{
+       res.render('list.ejs', { todoList : data }); //views에 넣어두어야됨
+   });  // 이렇게 하면 모두 가져옴.. 
+});
+
+/** /delete/#{id}  : 특정 TODOList 삭제 */ 
+app.delete('/posts/:id', (req, res) => {
+    req.body._id = parseInt(req.body._id);
+    db.collection("post").deleteOne(req.body, (err, data)=>{
+        if (err) throw err;
+        console.log("1 document deleted");
+        res.send('삭제완료');
+    });
+})
