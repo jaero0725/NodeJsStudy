296f95d73762fabb687ef9f270fe29277253b9a0
choijaeho <dhqkgbs@naver.com>
2022-12-20 00:10:29 +0900

    test : excel 받아서 api request 사용 테스트

--- WORD/server.js
+++ WORD/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+    limit:"50mb",
+    extended: false
+}));
 app.set('view engine','ejs');       
 require('dotenv').config()
 
 const request = require('request')
-
 
 // 한글 자모자 분리 library
 const Hangul = require('hangul-js');
@@ -24,8 +26,8 @@ app.get('/', (req, res) => {
 
 //검색
 app.post('/search', (req, res) => {
-    let a  = req.body.text;
-    console.log("# req.body : " , a);
+    let text  = req.body.text;
+    console.log("# req.body : " , text);
     console.log("process.env.PORT", process.env.KOREAN_KEY);
 
     //api 를 통해 보내기 -  검색어(UTF-8 인코딩)
@@ -46,4 +48,35 @@ app.post('/search', (req, res) => {
 
         res.status(200).send({message : word_definition});
       });
+});
+
+app.post('/excel', (req, res) => {
+    let excel  = JSON.parse(req.body.excel); 
+
+    //api 를 통해 보내기 -  검색어(UTF-8 인코딩)
+    const korean_url = "https://stdict.korean.go.kr/api/search.do?";
+    const korean_addr = 'certkey_no=3529&key=' + process.env.KOREAN_KEY + '&type_search=search&req_type=json&q=' ;
+    const korean_q =  encodeURI(req.body.text);
+    const api_req = korean_url+korean_addr+korean_q;
+
+    var word_definition = "";
+    var wordJson = new Array() ;
+    //for(idx in excel){
+        let data = new Object() ;
+        data.word = excel[0].word; 
+/*
+        async () => {
+            request(api_req, function(error, response, res){
+                if(error){  console.log(error)  }
+                console.log(res);
+                var obj = res;
+                var data = obj.channel.item;
+                word_definition = obj.channel.item[0].sense.definition;
+                data.definition = word_definition;            
+            });
+        }
+        */
+        wordJson.push(data) ;
+    //}
+    console.log("wordJson: ", wordJson);
 });
